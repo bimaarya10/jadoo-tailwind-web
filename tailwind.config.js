db67175eeb3ce0ce7b8ac351cf9a12b28a004cc9
Bima arya sena <bimaaryasena7@gmail.com>
2023-09-04 23:48:53 +0800

    portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors :{
+        primary : "#181E4B",
+        dark : "#64748b"
+      }
+    },
+  },
+  plugins: [],
+}
+
